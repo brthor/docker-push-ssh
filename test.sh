9247611f82177945ed196355b9548c3f15438f11
Bryan Thornbury <bt.mozoby@gmail.com>
2019-08-26 22:52:06 -0700

    add test for -r

--- test.sh
+++ test.sh
@@ -44,7 +44,8 @@ set -e
 [ "$failResult1" != "0" ]
 [ "$failResult2" != "0" ]
 
-docker-push-ssh --prime alpine -i ./emptykey -p 22 root@${hostIp} ${image1} ${image2}
+docker-push-ssh --prime alpine -i ./emptykey -p 22 root@${hostIp} ${image1}
+docker-push-ssh --prime alpine -r 5001 -i ./emptykey -p 22 root@${hostIp} ${image2}
 
 ssh  -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p 22 root@${hostIp} 'sh -l -c "docker run "'"${image1}"
 ssh  -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p 22 root@${hostIp} 'sh -l -c "docker run "'"${image2}"
